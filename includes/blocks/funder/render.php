9958af2f89db70fdb85d31b40213b9974229c2ca
alastair38 <alastair@alastaircox.com>
2025-04-17 15:00:12 +0100

    Updated profile and funder blocks. + grants relationship field to funder CPT.

--- includes/blocks/funder/render.php
+++ includes/blocks/funder/render.php
@@ -27,69 +27,81 @@ if( !empty($block['align']) ) {
 
 if(function_exists('get_field')):
   
-  $funder = get_field('funder');
-
-  $grants = get_posts( array(
-    'orderby'        => 'title',
-    'post_type'      => 'grant',
-    'grant-funder'   => $funder->name,
-  ));
+  $funders = get_field('funder');
 
 endif;
 
-  
-  if( $funder ): 
-    
-    $logo = get_field('funder_logo', 'term_'.$funder->term_id);
-   
-  // echo get_the_post_thumbnail($funder->ID, array( 80, 80), ['class' => 'rounded-full w-20 h-20 aspect-square object-cover'] );
-  ?>
-  <div id="funder-<?php echo $funder->term_id;?>" class="space-y-6 p-6 shadow-md rounded-md">
-    <div class="flex items-center gap-3">
-   
-    <?php if($logo):?>
-    <figure class="flex h-8 w-8 md:h-20 md:w-20 p-2 items-center">
-    <img class="object-contain h-full w-full"  src="<?php echo $logo['sizes']['medium'];?>" />
-    </figure>
-    <?php endif;?>
-     <h2 class="font-black leading-tight"><?php echo $funder->name; ?></h2>
-  </div>
-    <?php if($grants):?>
-     <div class="flex flex-col gap-6">
-     
-      <?php foreach($grants as $grant):
-        $projects = get_field('projects', $grant->ID);?>
+if( !empty($funders) ): ?>
+  <div class="space-y-6">
+  <?php foreach( $funders as $post ): 
+
+      // Setup this post for WP functions (variable must be named $post).
+      setup_postdata($post); 
+      
+      $description = get_field('funder_description', $post->ID);
+      $funder_link = get_field('funder_link', $post->ID);
+      $grants = get_field('funded_grants', $post->ID);
+      
+      ?>
+      <div class="rounded-md border p-6 space-y-6">
         
-        <div class="rounded-md bg-neutral-light-100 p-6 space-y-6">
-          <div class="space-y-3">
-          <h3 class="font-black flex flex-row-reverse flex-wrap justify-between"><span class="hidden md:flex text-sm font-normal bg-neutral-dark-900 text-white px-3 py-1 rounded-full">Grant</span><?php echo $grant->post_title;?></h3>
-          <p><?php echo get_the_excerpt( $grant->ID );?></p>
-       </div>
-          <hr aria-hidden="true" class="border-neutral-light-900">
+        <div class="space-y-6">
+          
+          <?php if(has_post_thumbnail( )):
+            
+            echo get_the_post_thumbnail($post->ID, 'full', array('class' => 'w-44 object-cover'));
+            
+            endif; ?>
+            
+          <h2 class="font-black"><?php echo get_the_title($post->ID); ?></h2>
+          
+          <?php if($description):?>
+            
+            <p><?php echo $description;?></p>
+            
+            <?php endif;?>
+            
+            <?php if($funder_link):?>
+              
+              <a rel="external" target="_blank" class="rounded-md text-sm inline-block w-fit bg-contrast text-white px-3 py-1 hover:ring-1 no-underline hover:text-white focus:ring-1 duration-100 ring-offset-2 ring-transparent hover:ring-contrast focus:ring-contrast" href="<?php echo $funder_link; ?>">
+              Funder homepage
+              </a>
+              
+            <?php endif;?>
+         
+        </div>
         
-        <?php 
-        if($projects):?>
-        <div class="space-y-3 ">
-            <p>Funded Projects</p>
-        <?php foreach($projects as $project):?>
-          
-          
-            <a class="flex text-sm" href="<?php the_permalink( $project->ID );?>"><?php echo $project->post_title;?></a>
-    
-        <?php endforeach;?>
+          <?php
+          if(! empty($grants)):?>
+          <hr aria-hidden="true" class="border-neutral-light-100">
+          <details class="space-y-3 text-sm">
+            <summary class="w-fit cursor-pointer flex items-center gap-1">
+              Funded Grants
+              <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+	              <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m8.25 4.5l7.5 7.5l-7.5 7.5" />
+              </svg>
+            </summary>
+            <div class="p-4 rounded-md space-y-3 bg-neutral-light-100">
+            <?php foreach($grants as $grant):?>
+              
+              <p>
+                <a href="<?php echo get_permalink($grant->ID); ?>">
+                  <?php echo get_the_title($grant->ID); ?>
+                </a>
+            </p>
+            
+            <?php endforeach;?>
             </div>
-        <?php endif; ?>
+            </details>
+          <?php endif;?>
           
-         </div>
-      <?php endforeach;?>
-   </div>
-    <?php endif;?>
-    
+      </div>
+  <?php endforeach; ?>
   </div>
-  
-
-<?php wp_reset_postdata(); endif; ?>
-
+  <?php 
+  // Reset the global post object so that the rest of the page works correctly.
+  wp_reset_postdata(); ?>
+<?php endif; ?>
   
   
   
--- includes/blocks/profile/render.php
+++ includes/blocks/profile/render.php
@@ -24,55 +24,29 @@ if( !empty($block['align']) ) {
     $className .= ' align' . $block['align'];
 }
 
-
 $profile = get_field('profile');
 $simplified = get_field('simplified'); 
 ?>
 
-
-<div id="profile-<?php echo $profile->ID;?>" class="flex items-center md:items-start flex-col p-3 md:p-6 mb-6 w-full h-full border rounded-md gap-6 shadow-md">
+<div id="profile-<?php echo $profile->ID;?>" class="flex flex-col items-center justify-between p-3 md:p-6 mb-6 w-full h-full border rounded-md gap-6">
  
-  <div class="flex flex-col md:flex-row gap-6 items-center">
   <?php
   
-  
   if( $profile ): 
-  echo get_the_post_thumbnail($profile->ID, array( 80, 80), ['class' => 'rounded-full w-20 h-20 aspect-square object-cover'] );?>
-  <div class="text-center md:text-left">
+  echo get_the_post_thumbnail($profile->ID, array( 80, 80), ['class' => ' rounded-full w-20 h-20 aspect-square object-cover'] );?>
+  
+  <div class="text-center">
+    
     <h3 class="font-black"><?php echo esc_html( $profile->post_title ); ?></h3>
     <p class="text-sm"><?php echo esc_html( get_field('work_title', $profile->ID) ); ?></p>
+    
   </div>
-  
-  </div>
-  
+ 
+  <?php endif; ?>
 
-<?php endif; ?>
-
-  
-  <a class="gap-2 ml-auto mr-auto md:ml-0 w-fit no-underline items-center bg-contrast text-white text-sm px-3 py-1 rounded-full hover:ring-2 focus:ring-2 ring-offset-2 ring-transparent hover:ring-contrast focus:ring-contrast" href="<?php echo get_the_permalink( $profile->ID ); ?>">Further information
-  
+  <a class="gap-2 mx-auto w-fit no-underline items-center bg-contrast text-white text-sm px-3 py-1 rounded-full hover:ring-1 focus:ring-1 ring-offset-2 ring-transparent hover:ring-contrast focus:ring-contrast" href="<?php echo get_the_permalink( $profile->ID ); ?>">
+    <?php esc_html_e( 'OCNS Profile', 'blockhaus' );?>
   </a>
-
-
-
-
-<!-- <?php $projects = get_field('projects', $profile->ID);
-  if( $projects && !$simplified ): ?>
-  <hr aria-hidden="true" class="border-neutral-light-900">
-  <div class="flex flex-col gap-4">
-    <span class="text-sm font-black">Projects</span>
-      <div class="grid grid-cols-1 gap-2 flex-wrap">
-      <?php foreach( $projects as $post ): ?>
-        
-        <a class="flex gap-2 text-sm underline flex-wrap items-center bg-contrasttext-whitetext-smpx-3py-1rounded-fullhover:ring-2 focus:ring-2ring-offset-2 ring-transparenthover:ring-contrastfocus:ring-contrast" href="<?php echo get_the_permalink($post->ID); ?>">
-          <?php echo get_the_title($post->ID); ?>
-        </a>
-        
-      <?php endforeach; ?>
-      </div>
-  </div>
-
-<?php wp_reset_postdata(); endif; ?> -->
 
 </div>
   
